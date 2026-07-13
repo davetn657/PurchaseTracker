9ef34c29b12805d2b19b753866c823d3a85cca94
Dave Ngo <davetn657@gmail.com>
2026-07-12 20:56:15 -0400

    Dto for purchaseItems created

---
+++ PurchaseTracker.Api/Dtos/PurchaseItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PurchaseTracker.Api.Dtos;
+
+public class PurchaseItemDto
+{
+    public string ItemName { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public DateTime PurchaseDate { get; set; } = DateTime.Now;
+    public string PaymentStatus { get; set; } = "Paid";
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal PaymentPrice { get; set; }
+}
